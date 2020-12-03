fba5f112666b6b07d2eb9f9ccbe7a6e4496fbb69
ppm-manoj <manoj.kumar@paypermint.in>
2020-12-03 19:57:44 +0530

    added flag

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "github.com/paypermint/mskit"
+import (
+	"flag"
+	"github.com/paypermint/mskit"
+)
 
 const (
 	serviceName = "bridge-app-svc"
@@ -8,4 +11,15 @@ const (
 
 func main() {
 	mskit.Init(serviceName)
+	mskit.RequireFlags(mskit.F_GRPC | mskit.F_LOG | mskit.F_HEALTH | mskit.F_KEY | mskit.F_REGION)x
+	mskit.LoadValidators()
+	flag.Parse()
+
+	config := mskit.GetConfig()
+
+	log := mskit.NewLogger(config.Log)
+	sctx := mskit.NewServiceContext(config, log)
+	defer sctx.Cleanup()
+
+	go mskit.StartHealthCheckEndpoint(sctx)
 }
