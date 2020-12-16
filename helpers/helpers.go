ca7a9a1184c66fc9dfed894c06f87c5d79394595
ppm-avinder <avinder.singh@paypermint.in>
2020-12-16 12:47:19 +0530

    This refers paypermint/IssuesAndDefects#802 - Added code for payment update notification api call to CIL

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -250,3 +250,85 @@ func GetOptionalStringParam(params map[string]string, key string) (string, error
 	}
 	return EmptyString, nil
 }
+
+//HasUnsupportedInterfaceParameters returns true if the data map contains a key not present in the list of keys
+func HasUnsupportedInterfaceParameters(data map[string]interface{}, keys ...string) (string, bool) {
+	expected := map[string]bool{}
+	for _, key := range keys {
+		expected[key] = true
+	}
+	for key := range data {
+		if !expected[key] {
+			return key, true
+		}
+	}
+	return EmptyString, false
+}
+
+//GetStringInterfaceParam returns the value of key in params
+//TODO refactor
+func GetStringInterfaceParam(params map[string]interface{}, key string) (string, error) {
+	if value, ok := params[key]; ok {
+		if value == EmptyString || (key == util.KeyCurrency && params[key] != "INR") {
+			return EmptyString, errors.New(util.InvalidPostParameterMsg)
+		}
+		return value.(string), nil
+	}
+	return EmptyString, errors.New(util.MissingMandatoryField)
+}
+
+//GetParamsWithRecordRequest returns the api request params as an map of key to interface
+func GetParamsWithRecordRequest(r *http.Request, itemKey string) (map[string]interface{}, string, error) {
+	params := make(map[string]interface{})
+	var field string
+	var err error
+	errPayload := errors.New("Error in payload formation")
+	contentType := getContentTypeFromRequest(r.Header)
+
+	switch contentType {
+	case jsonHeader:
+		if itemKey == util.KeyRecords {
+			params, field, err = getRecordParamsForJSON(r, itemKey)
+		}
+
+	default:
+		return nil, field, errPayload
+	}
+	if err != nil {
+		return nil, field, err
+	}
+	return params, field, nil
+}
+
+//GetRecordsParam creates a record request from param map
+func GetRecordsParam(params map[string]interface{}, recordKey string, optional bool) ([]*Record, error) {
+	var recordRequests []*Record
+	if recordItems, ok := params[recordKey]; ok {
+		switch vv := recordItems.(type) {
+		case []*Record:
+			if len(vv) == 0 {
+				return nil, errors.New(util.InvalidPostParameterMsg)
+			}
+			for _, record := range vv {
+				recordRequests = append(recordRequests, &Record{
+					CustomerNumber: record.CustomerNumber,
+					CustomerName:   record.CustomerName,
+					CompanyCode:    record.CompanyCode,
+					Description:    record.Description,
+					Item:           record.Item,
+					AmountDue:      record.AmountDue,
+					PaymentAmount:  record.PaymentAmount,
+					BankAccount:    record.BankAccount,
+					TransactionRef: record.TransactionRef,
+				})
+			}
+		default:
+			return nil, fmt.Errorf("Unknown type for %s", vv)
+		}
+	} else {
+		if !optional {
+			return nil, errors.New(util.MissingMandatoryField)
+		}
+	}
+	return recordRequests, nil
+}
--- helpers/payment.go
+++ helpers/payment.go
@@ -3,10 +3,15 @@ package helpers
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
 	"net/http"
+	"reflect"
+	"strconv"
 
 	"github.com/paypermint/bridge-app-svc/models"
+	"github.com/paypermint/bridge-app-svc/util"
 )
 
 //PostPaymentUpdateRequest represents struct to update payments at SAP end
@@ -44,4 +49,142 @@ func (c *Client) PostPaymentUpdateToSAP(paymentUpdateRequest *PostPaymentUpdateR
 	}
 
 	return response, nil
+}
+
+func getRecordParamsForJSON(r *http.Request, itemKey string) (map[string]interface{}, string, error) {
+	params := make(map[string]interface{})
+	var f interface{}
+	dec := json.NewDecoder(r.Body)
+	dec.UseNumber()
+	err := dec.Decode(&f)
+	if err != nil && err != io.EOF {
+		return params, EmptyString, err
+	}
+	if f == nil {
+		return params, EmptyString, nil
+	}
+
+	m, ok := f.(map[string]interface{})
+	if !ok {
+		return nil, EmptyString, errors.New("Can not convert to map")
+	}
+	value, ok := m[itemKey]
+	if ok && reflect.TypeOf(value).Kind() != reflect.Slice {
+		return nil, util.KeyRecords, errors.New(util.InvalidPostParameterMsg)
+	}
+
+	for k, v := range m {
+		switch vv := v.(type) {
+		case string:
+			params[k] = vv
+		case bool:
+			params[k] = strconv.FormatBool(vv)
+		case json.Number:
+			params[k] = vv.String()
+		case map[string]interface{}:
+			jsonString, err := json.Marshal(v)
+			if err != nil {
+				return nil, k, err
+			}
+			params[k] = string(jsonString)
+		case []interface{}:
+			switch k {
+			case itemKey:
+				recordItems, field, err := getRecordItemParamsForJSON(vv)
+				if err != nil {
+					return nil, field, err
+				}
+				params[k] = recordItems
+			default:
+				return nil, k, errors.New(util.InvalidPostParameterMsg)
+			}
+		default:
+			return nil, k, errors.New(util.InvalidPostParameterMsg)
+		}
+	}
+	return params, EmptyString, nil
+}
+
+func getRecordItemParamsForJSON(values []interface{}) ([]*Record, string, error) {
+	var records []*Record
+
+	for _, val := range values {
+		// val is an interface. which contains map[string]interface{} and for note its map[string](map[string]interface{})
+
+		switch vFurther := val.(type) {
+		case map[string]interface{}:
+			if field, ok := HasUnsupportedInterfaceParameters(vFurther, util.KeyCustomerNumber, util.KeyCustomerName, util.KeyCompanyCode, util.KeyItem, util.KeyAmountDue,
+				util.KeyDescription, util.KeyPaymentAmount, util.KeyBankAccount, util.KeyTransactionRef); ok {
+				return nil, field, errors.New(util.UnsupportedParamMsg)
+			}
+			record := &Record{}
+			customerNumber, err := GetStringInterfaceParam(vFurther, util.KeyCustomerNumber)
+			if err != nil {
+				return nil, util.KeyCustomerNumber, err
+			}
+			delete(vFurther, util.KeyCustomerNumber)
+			customerName, err := GetStringInterfaceParam(vFurther, util.KeyCustomerName)
+			if err != nil {
+				return nil, util.KeyCustomerName, errors.New(util.InvalidPostParameterMsg)
+			}
+			delete(vFurther, util.KeyCustomerName)
+			companyCode, err := GetStringInterfaceParam(vFurther, util.KeyCompanyCode)
+			if err != nil {
+				return nil, util.KeyCompanyCode, err
+			}
+			delete(vFurther, util.KeyCompanyCode)
+			description, err := GetStringInterfaceParam(vFurther, util.KeyDescription)
+			if err != nil {
+				return nil, util.KeyDescription, err
+			}
+			delete(vFurther, util.KeyDescription)
+			item, err := GetStringInterfaceParam(vFurther, util.KeyItem)
+			if err != nil {
+				return nil, util.KeyItem, err
+			}
+			delete(vFurther, util.KeyItem)
+			amountDue, err := GetStringInterfaceParam(vFurther, util.KeyAmountDue)
+			if err != nil {
+				return nil, util.KeyAmountDue, err
+			}
+			delete(vFurther, util.KeyAmountDue)
+			paymentAmount, err := GetStringInterfaceParam(vFurther, util.KeyPaymentAmount)
+			if err != nil {
+				return nil, util.KeyPaymentAmount, err
+			}
+			delete(vFurther, util.KeyPaymentAmount)
+			bankAccount, err := GetStringInterfaceParam(vFurther, util.KeyBankAccount)
+			if err != nil {
+				return nil, util.KeyBankAccount, err
+			}
+			delete(vFurther, util.KeyBankAccount)
+			transActionRef, err := GetStringInterfaceParam(vFurther, util.KeyTransactionRef)
+			if err != nil {
+				return nil, util.KeyTransactionRef, err
+			}
+			delete(vFurther, util.KeyTransactionRef)
+			record.CustomerNumber = customerNumber
+			record.CustomerName = customerName
+			record.CompanyCode = companyCode
+			record.Description = description
+			record.Item = item
+			record.AmountDue = amountDue
+			record.PaymentAmount = paymentAmount
+			record.BankAccount = bankAccount
+			record.TransactionRef = transActionRef
+
+			jsonString, err := json.Marshal(vFurther)
+			if err != nil {
+				return nil, util.KeyRecords, errors.New(util.InvalidPostParameterMsg)
+			}
+
+			err = json.Unmarshal(jsonString, record)
+			if err != nil {
+				return nil, util.KeyRecords, errors.New(util.InvalidPostParameterMsg)
+			}
+
+			records = append(records, record)
+		}
+	}
+	return records, "", nil
 }
